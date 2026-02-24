c63bdb025728c3ef4cf008e807e36a4f8974e0a5
GitGor <131777823+SeanGordaev@users.noreply.github.com>
2026-02-24 23:22:54 +0200

    Create input.js

---
+++ src/components/input.js
@@ -0,0 +1,46 @@
+import '../styles/input.css'
+import { createContext, useContext, useEffect, useState } from 'react'
+import { context } from './father.js'
+
+
+export const last = createContext();
+
+export const Input = (props) => {
+
+    const [tasksInfo, changeTasks] = useContext(context);
+    const [done, setDone] = useState(0);
+    const [text, setText] = useState('');
+
+    const textIsNotEmpty = (text) => {
+        return /[a-zA-Z0-9]/.test(text)
+    }
+
+    useEffect(() => {
+        let count = 0;
+
+        tasksInfo.forEach(v => {
+            v.done === true && count++;
+        });
+
+        setDone(count);
+    }, [tasksInfo]); 
+
+    const AddTask = () => {
+        if (textIsNotEmpty(text)) {
+            changeTasks([...tasksInfo, 
+                {
+                    id: crypto.randomUUID(),
+                    text: text,
+                    done: false
+                }
+            ]);
+        }
+    }
+
+    return (
+    <div id="Input" className='ui'>
+        <input id='user-tast' onChange={e => setText(e.target.value)}/>
+        <button onClick={() => AddTask()}>Add</button>
+        <span>{done}/{tasksInfo.length}</span>
+    </div>)
+}
