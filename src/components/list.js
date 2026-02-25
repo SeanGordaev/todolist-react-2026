3b767c25023ed0584da151988235b161c09fe3db
GitGor <131777823+SeanGordaev@users.noreply.github.com>
2026-02-25 16:30:08 +0200

    Update list.js

--- src/components/list.js
+++ src/components/list.js
@@ -1,20 +1,17 @@
 import "../styles/list.css"
 import { Item } from './element.js'
-import { useContext, useState } from 'react'
+import { useContext } from 'react'
 import { context } from './father.js'
 
 
 export const List = () => {
 
-    const [tasksInfo, changeTasks] = useContext(context);
+    const [tasksInfo] = useContext(context);
 
     return (
         <div className='list'>
         {
-        Array.from({ length:tasksInfo.length }, (_, i) => i).map(
-            i => (
-                <Item key={tasksInfo[i].id} object={tasksInfo[i]} className='created'/>)
-            )
+            tasksInfo.map(obj => (<Item key={obj.id} object={obj} className='created'/>))
         }
         </div>
     )
